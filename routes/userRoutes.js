b3a2208051433b86be758e047208262c4e9b97a8
mohdmaaz <mohdmaazshk@gmail.com>
2024-11-14 10:33:05 +0530

    Login route added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createUserController } from "../controllers/userController.js";
+import { createUserController, loginUserController} from "../controllers/userController.js";
 
 const router = express.Router();
 
 // Define the route for creating a user
 router.post("/auth/register", createUserController);
 
+// User login route
+router.post("/auth/login",loginUserController)
 
 export default router;
