7fc348a80123f417464fb1a0e248d493f967f4f7
mohdmaaz <mohdmaazshk@gmail.com>
2024-11-14 10:31:06 +0530

    Jwt token added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,52 @@
-import { createUser } from "../services/userService.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+import { createUser, findUserByEmail } from "../services/userService.js";
 
 export const createUserController = async (req, res) => {
   try {
     const userData = req.body;
     const newUser = await createUser(userData);
-    res.status(201).json({ message: "User created successfully", user: newUser });
+    res
+      .status(201)
+      .json({ message: "User created successfully", user: newUser });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    next(error);
+  }
+};
+
+export const loginUserController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await findUserByEmail(email);
+
+    if (!user) {
+      const error = new Error("Invalid email id");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (!validPassword) {
+      const error = new Error("Invalid password");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    next(error);
   }
 };
